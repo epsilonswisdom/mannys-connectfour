251eeb720899306a02468f8f28e26b0eb548947d
Manny <emaldonado1402@gmail.com>
2022-12-16 13:48:08 -0500

    Present Psuedo Code

---
+++ app.js
@@ -0,0 +1,54 @@
+/*------------ Constants ------------*/
+
+
+/*------------ Variables ------------*/
+
+
+/*---- Cached Element References ----*/
+
+
+/*------------ Functions ------------*/
+
+
+
+
+
+// 1)Define the needed variables to keep track of the state of the game.
+// 1a)variables that should be defined
+// cnt4board
+// winner
+//loser maybe not cause if you have two truthys you can make loser a falsy
+// tie
+
+
+// 2)Store the cached elements 
+// name constants I wanna make them circles so circEls youre gonna need to store 7x6 so it would be 42 circles
+// we want messages to appear so make a msg element.
+// set up click and btns and event listeners as well but after our base script is set up 
+// 
+
+
+//3) try loading the game, see if its rendering. User should be able to view it Don't forget to add some css so youre not looking at a white board.
+//a)Create the needed functions to render the board. so you need to store an array for the 42 elements
+
+//b)create a render function to see if the board is rendered above is repeated but more research is req.
+
+
+// c)css imagine the board make it a grid of (Length by Width) 7 x 6 thats how many playable spaces should be
+
+
+
+
+//4)Render the board console.log to make sure once that happens you can start using functions to add using the variables
+
+//a)you need functions for a tie, taking turns, a winner, a loser, a random math generator so adding a computer too for single players.
+
+//b) you wanna make functions for the changing state of the game.
+// write out winning combos. Diagnally and linear connecting four in a row  I think maybe more than 8 ways to win?
+
+// c) add evt listeners for handling actions like player click then write functions for input by the user.
+
+// d)you also want to check for chip placements check for winner, tie and loser, as well player turns
+
+// e)if game works make a reset button
+//5 Make it pretty for Ben not confetti maybe stars going everywhere

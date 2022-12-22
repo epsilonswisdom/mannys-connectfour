f266b092d5e6bee242c1659d540b30a1af47df3c
Manny <emaldonado1402@gmail.com>
2022-12-22 15:36:38 -0500

    Error return undefine line 97

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 /*------------ Constants(winning combos) ------------*/
-
+let winningCombos = [ 
+  [0, 1, 2, 3], [41, 40, 39, 38],[7, 8, 9, 10], 
+  [34, 33, 32, 31], [14, 15, 16, 17], [27, 26, 25, 24], 
+  [21, 22, 23, 24], [20, 19, 18, 17], [28, 29, 30, 31], 
+  [13, 12, 11, 10], [35, 36, 37, 38], [6, 5, 4, 3], 
+  [0, 7, 14, 21], [41, 34, 27, 20], [1, 8, 15, 22], 
+  [40, 33, 26, 19], [2, 9, 16, 23], [39, 32, 25, 18], 
+  [3, 10, 17, 24], [38, 31, 24, 17], [4, 11, 18, 25], 
+  [37, 30, 23, 16], [5, 12, 19, 26], [36, 29, 22, 15], 
+  [6, 13, 20, 27], [35, 28, 21, 14], [0, 8, 16, 24], 
+  [41, 33, 25, 17], [7, 15, 23, 31], [34, 26, 18, 10], 
+  [14, 22, 30, 38], [27, 19, 11, 3], [35, 29, 23, 17], 
+  [6, 12, 18, 24], [28, 22, 16, 10], [13, 19, 25, 31], 
+  [21, 15, 9, 3], [20, 26, 32, 38], [36, 30, 24, 18], 
+  [5, 11, 17, 23], [37, 31, 25, 19], [4, 10, 16, 22], 
+  [2, 10, 18, 26], [39, 31, 23, 15], [1, 9, 17, 25], 
+  [40, 32, 24, 16], [9, 7, 25, 33], [8, 16, 24, 32], 
+  [11, 7, 23, 29], [12, 18, 24, 30], [1, 2, 3, 4], 
+  [5, 4, 3, 2], [8, 9, 10, 11], [12, 11, 10, 9],
+  [15, 16, 17, 18], [19, 18, 17, 16], [22, 23, 24, 25], 
+  [26, 25, 24, 23], [29, 30, 31, 32], [33, 32, 31, 30], 
+  [36, 37, 38, 39], [40, 39, 38, 37], [7, 14, 21, 28], 
+  [8, 15, 22, 29], [9, 16, 23, 30], [10, 17, 24, 31], 
+  [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34] 
+  ];
 
 /*------------ Variables (State of the Game) ------------*/
 let winner 
@@ -9,14 +33,14 @@ let turn
 
 /*---- Cached Element References ----*/
 const tileEls = document.querySelectorAll(".tile")
-console.dir(tileEls)
-//const columnEls = document.querySelectorAll()
-const introEl = document.querySelector("#intro")
-console.dir(introEl)
-const messageEl = document.querySelector("#message")
-console.dir(messageEl)
+
+
+const introEl = document.querySelector("intro")
+
+const messageEl = document.getElementById("message")
+
 /*----------Event Listeners-----------*/
-document.addEventListener("click", handleClick)
+document.querySelector(".connectFour").addEventListener("click", handleClick)
 
 /*------------ Functions ------------*/
 function init() {
@@ -25,7 +49,7 @@ function init() {
   winner = false;
   tie = false;
   render()
-
+  
 }
 
 function render(){
@@ -45,13 +69,13 @@ function updateBoard() {
       "Black";
       return
      }
-      else  (tile === null) {
+      else  (tile === null); {
       tileEls[index].textContent =
       "";
       return
-     }
+      }
   })
-  updateBoard()
+  
 }
 // //update Msg
 function updateMessage() {
@@ -66,28 +90,28 @@ function updateMessage() {
   } else {
     messageEl.textContent = "It's a Bloody Tie How?"
   }
-  updateMessage()
+  
 }
 function handleClick(evt) {
-  let tileIdx = +evt.target.id.replace("tile", "")
-  if (board(tileIdx) !== null) {
+let tileIdx = +evt.target.id.replace(".tile","")
+if (board[tileIdx] !== null) {
       return
-  } else if(winner === true) {
+}  else if(winner === true) {
     return
-  }
+}
+placePiece(tileIdx)
+checkForTie()
+checkForWinner()
+switchPlayerTurn()
+render()
   
   
-  placePiece(tileIdx)
-  checkForTie()
-  checkForWinner()
-  switchPlayerTurn()
-  render()
 }
 // //above is correct
 function placePiece(index){   
-  for (let i =(index + 35); i > -1; i = 7){
-    if(board.includes(null)){ 
-      return board[index] = turn 
+  for (let i = (index + 35); i > -1; i = 7){
+    if(board[i] === null){ 
+      return board[i] = turn 
 
     
   
@@ -104,10 +128,11 @@ function checkForTie() {
 
 function checkForWinner(){
   winningCombos.forEach(function(winCombos) {
-    let sum = winCombos.reduce(function(alpha,num){
-      return alpha + board[num]
-    },0);
-      if(Math.abs(sum)===3){
+    let sum = 0
+    winCombos.forEach(function(index){
+      sum += board[index]
+    })
+      if(sum === 4 || sum === -4){
         winner = true
       }
   })
